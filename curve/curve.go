072dbbde4a8c73086e900c0307822ae138ac3d27
Minke Zhang <minke.zhang@gmail.com>
2020-10-13 22:50:59 -0700

    Add LinearMoveCurve

--- curve/curve.go
+++ curve/curve.go
@@ -6,15 +6,34 @@ import (
 	gcpb "github.com/downflux/game/api/constants_go_proto"
 )
 
+// Curve represents the evolution of a specific data metric over time, e.g. HP,
+// unit orientation, position, etc.
+//
+// TODO(minkezhang): Translate to generics instead. See
+// https://blog.golang.org/generics-next-step.
+//
+// type Curve[T any] interface {
+//   Get(t float64) T
+// }
+//
+// We can remove the DatumType function once this is implemented.
 type Curve interface {
 	Type() gcpb.CurveType
 	ID() string
 	DatumType() reflect.Type
-	ClientID() string
-	Get(t float32) interface{} // interpolation
-}
+	EntityID() string
 
-func IsSubsetOf(c1, c2 Curve) bool
-func Hash(c Curve) bool // placeholder type
-func Merge(c1, c2 Curve) error
-func Extract(t1, t2 float32) Curve // CurveID is the same
+	// Add takes a value and copies it into the curve.
+	Add(t float64, v interface{}) error
+
+	// Get returns a copy of the interal value at a given tick.
+	Get(t float64) (interface{}, error)
+
+	// TODO(minkezhang): Implement the following.
+	/**
+	 * Merge(c Curve) error
+	 * Extract(t1, t2 float32) Curve // Same ID
+	 * Contains(c Curve) bool
+	 * Hash() bool // placeholder type
+	 */
+}
---
+++ curve/linearmove.go
@@ -0,0 +1,81 @@
+package linearmove
+
+import (
+	"reflect"
+	"sort"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	gcpb "github.com/downflux/game/api/constants_go_proto"
+	gdpb "github.com/downflux/game/api/data_go_proto"
+)
+
+const (
+	curveType = gcpb.CurveType_CURVE_TYPE_LINEAR_MOVE
+)
+
+var (
+	datumType = reflect.TypeOf(&gdpb.Coordinate{})
+)
+
+type datum struct {
+	tick  float64
+	value *gdpb.Coordinate // clone, immutable
+}
+
+func datumBefore(d1, d2 datum) bool {
+	return d1.tick < d2.tick
+}
+
+func insert(data []datum, d datum) []datum {
+	i := sort.Search(len(data), func(i int) bool { return !datumBefore(data[i], d) })
+	// Override existing value if the given input will result in an invalid
+	// function.
+	if i < len(data) && data[i].tick == d.tick {
+		data[i] = d
+	} else {
+		data = append(data, datum{})
+		copy(data[i+1:], data[i:])
+		data[i] = d
+	}
+	return data
+}
+
+type LinearMoveCurve struct {
+	id       string
+	entityID string
+	data     []datum
+}
+
+func (c *LinearMoveCurve) Type() gcpb.CurveType    { return curveType }
+func (c *LinearMoveCurve) ID() string              { return c.id }
+func (c *LinearMoveCurve) DatumType() reflect.Type { return datumType }
+func (c *LinearMoveCurve) ClientID() string        { return c.entityID }
+
+func (c *LinearMoveCurve) Add(t float64, v interface{}) error {
+	c.data = insert(c.data, datum{tick: t, value: v.(*gdpb.Coordinate)}) // copy
+	// TODO(minkezhang): Add data validation.
+	return nil
+}
+
+func (c *LinearMoveCurve) Get(t float64) (interface{}, error) {
+	if c.data == nil || datumBefore(datum{tick: t}, c.data[0]) {
+		return nil, status.Error(codes.OutOfRange, "given tick occurs before the curve existed")
+	}
+
+	i := sort.Search(len(c.data), func(i int) bool { return !datumBefore(c.data[i], datum{tick: t}) })
+
+	if i == len(c.data) {
+		return proto.Clone(c.data[len(c.data)-1].value).(*gdpb.Coordinate), nil
+	}
+	if i == 0 {
+		return proto.Clone(c.data[0].value).(*gdpb.Coordinate), nil // proto.Clone
+	}
+
+	return &gdpb.Coordinate{
+		X: (c.data[i].value.GetX() + c.data[i-1].value.GetX()) / 2,
+		Y: (c.data[i].value.GetY() + c.data[i-1].value.GetY()) / 2,
+	}, nil
+}
