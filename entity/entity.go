d7dd85c7aa21683cafa300f4c61bbc6e527032f0
Minke Zhang <minke.zhang@gmail.com>
2020-10-14 01:14:42 -0700

    Add SimpleEntity placeholder for use in simple server loop

--- entity/entity.go
+++ entity/entity.go
@@ -9,3 +9,15 @@ type Entity interface {
 	Type() gcpb.EntityType
 	CurveID(t gcpb.CurveType) string
 }
+
+type SimpleEntity struct {
+	id string
+	curveLookup map[gcpb.CurveType]string
+}
+
+func NewSimpleEntity(id string) *SimpleEntity {
+	return &SimpleEntity{id: id, curveLookup: map[gcpb.CurveType]string{}}
+}
+func (e *SimpleEntity) ID() string { return e.id }
+func (e *SimpleEntity) Type() gcpb.EntityType { return gcpb.EntityType_ENTITY_TYPE_TANK }
+func (e *SimpleEntity) CurveID(t gcpb.CurveType) string { return e.curveLookup[t] }
