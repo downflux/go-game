234839837cfb41cade542b18e8d5bcf385222eb3
Minke Zhang <minke.zhang@gmail.com>
2020-10-18 02:56:45 -0700

    Increase charset for id.RandomString

--- server/id.go
+++ server/id.go
@@ -4,9 +4,9 @@ import (
 	"math/rand"
 )
 
-// TODO(minkezhang): Export to shared for server.
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
 func RandomString(n int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyz"
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
