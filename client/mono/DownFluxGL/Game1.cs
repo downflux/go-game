76f22ffb06ee32040145a8d4211bad044a55aa07
Minke Zhang <minke.zhang@gmail.com>
2020-10-20 01:50:03 -0700

    Update Game1 with basic tutorial logic

--- client/mono/DownFluxGL/Game1.cs
+++ client/mono/DownFluxGL/Game1.cs
@@ -6,12 +6,23 @@ namespace DownFluxGL
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            // TODO(minkezhang): Figure out why we can't control actual window
+            // size here.
+            _graphics.PreferredBackBufferWidth = 100;
+            _graphics.PreferredBackBufferHeight = 100;
+            _graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -19,6 +30,10 @@ namespace DownFluxGL
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            ballPosition = new Vector2(
+                _graphics.PreferredBackBufferWidth / 2,
+                _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -28,14 +43,43 @@ namespace DownFluxGL
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ballTexture = Content.Load<Texture2D>("Assets/ball");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            // TODO: Add your update logic here
+
+            KeyboardState state = Keyboard.GetState();
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (var key in state.GetPressedKeys())
+                sb.Append("Key: ").Append(key).Append(" pressed ");
+
+            if (sb.Length > 0)
+                System.Console.Error.WriteLine(sb.ToString());
+
+            if (state.IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (state.IsKeyDown(Keys.E))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if(state.IsKeyDown(Keys.D))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (state.IsKeyDown(Keys.S))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if(state.IsKeyDown(Keys.F))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if(ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if(ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if(ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
 
             base.Update(gameTime);
         }
@@ -45,6 +89,20 @@ namespace DownFluxGL
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
