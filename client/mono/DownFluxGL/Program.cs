42b9b5bb1dbd5065f8c87261032821cf3e9299bf
Minke Zhang <minke.zhang@gmail.com>
2020-10-21 01:38:16 -0700

    Link repo proto files into the client

--- client/mono/DownFluxGL/Program.cs
+++ client/mono/DownFluxGL/Program.cs
@@ -7,7 +7,7 @@ namespace DownFluxGL
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new DownFlux())
                 game.Run();
         }
     }
