9b48bf0009d24db8064a273c3237a0ade5d9114a
Minke Zhang <minke.zhang@gmail.com>
2020-11-04 20:31:45 -0800

    Add sample invocation path for hermetic toxiproxy

--- TODO/network/client.go
+++ TODO/network/client.go
@@ -4,16 +4,21 @@ import (
 	"context"
 	"io"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
         "google.golang.org/grpc"
         "google.golang.org/grpc/keepalive"
+	toxiproxyclient "github.com/Shopify/toxiproxy/client"
+	"github.com/Shopify/toxiproxy"
 
 	dpb "github.com/downflux/game/TODO/network/demo_go_proto"
 )
 
 const (
-	addr = "localhost:4445"
+	addr = "localhost:4444"
 	clientKeepAliveTime = 10 * time.Second
 	clientKeepAliveTimeout = 1 * time.Second
 )
@@ -29,6 +34,25 @@ var (
 )
 
 func main() {
+	tox := toxiproxy.NewServer()
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGTERM)
+	go func() {
+		<-signals
+		os.Exit(0)
+	}()
+
+	go func() {
+		tox.Listen("localhost", "50001")
+		select {}
+	}()
+
+	proxycli := toxiproxyclient.NewClient("localhost:50001")
+	p, err := proxycli.CreateProxy("downflux", "localhost:50000", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	conn, err := grpc.Dial(addr, grpc.WithInsecure(), dialOpts) // keepalive here
 	if err != nil {
 		log.Fatal(err)
@@ -45,7 +69,11 @@ func main() {
 	for {
 		log.Println("calling server stream")
 		r, err := stream.Recv()
-		log.Println("read from server stream")
+		log.Println("read from server stream, adding downstream")
+		p.AddToxic("latency_downstream", "latency", "downstream", 1.0, toxiproxyclient.Attributes{
+			"latency": 10000,
+		})
+		defer p.Delete()
 		if err != nil {
 			if err == io.EOF {
 				log.Println("received EOF, exiting normally")
