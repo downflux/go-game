80a316c81e276416162773526482f6af974b67f7
Minke Zhang <minke.zhang@gmail.com>
2020-11-24 23:05:25 -0800

    Fix remaining executor tests that were broken due to visitor pattern migration

--- server/service/executor_test.go
+++ server/service/executor_test.go
@@ -40,22 +40,22 @@ func TestNewExecutor(t *testing.T) {
 
 func TestAddEntity(t *testing.T) {
 	const (
-		t0 = float64(0)
+		t0       = float64(0)
 		nClients = 1000
 	)
 	src := &gdpb.Position{X: 0, Y: 0}
 
 	want := &apipb.StreamDataResponse{
-		Tick: t0 + 1,
+		Tick:     t0 + 1,
 		Entities: []*gdpb.Entity{{Type: gcpb.EntityType_ENTITY_TYPE_TANK}},
-                Curves: []*gdpb.Curve{{
-                        Type:     gcpb.CurveType_CURVE_TYPE_LINEAR_MOVE,
-                        Category: gcpb.CurveCategory_CURVE_CATEGORY_MOVE,
-                        Data: []*gdpb.CurveDatum{
-                                {Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 0, Y: 0}}},
-                        },
+		Curves: []*gdpb.Curve{{
+			Type:     gcpb.CurveType_CURVE_TYPE_LINEAR_MOVE,
+			Category: gcpb.CurveCategory_CURVE_CATEGORY_MOVE,
+			Data: []*gdpb.CurveDatum{
+				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 0, Y: 0}}},
+			},
 			Tick: t0 + 1,
-                }},
+		}},
 	}
 
 	e, err := New(simpleLinearMapProto, &gdpb.Coordinate{X: 2, Y: 1})
@@ -125,31 +125,14 @@ func TestAddEntity(t *testing.T) {
 
 // TODO(minkezhang): Test sending Move request on a stale tick -- what should
 // actually occur in the response?
-/*
-func TestDoTick(t *testing.T) {
+
+func TestDoMove(t *testing.T) {
 	const (
-		eid      = "entity-id"
-		t1       = float64(0)
+		t0       = float64(0)
 		nClients = 1000
 	)
 	dest := &gdpb.Position{X: 3, Y: 0}
 	src := &gdpb.Position{X: 0, Y: 0}
-
-	want := &apipb.StreamDataResponse{
-		Tick:     0,
-		Entities: []*gdpb.Entity{{EntityId: eid, Type: gcpb.EntityType_ENTITY_TYPE_TANK}},
-		Curves: []*gdpb.Curve{{
-			EntityId: eid,
-			Type:     gcpb.CurveType_CURVE_TYPE_LINEAR_MOVE,
-			Category: gcpb.CurveCategory_CURVE_CATEGORY_MOVE,
-			Data: []*gdpb.CurveDatum{
-				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 0, Y: 0}}},
-				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 1, Y: 0}}},
-				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 2, Y: 0}}},
-				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 3, Y: 0}}},
-			},
-		}},
-	}
 
 	e, err := New(simpleLinearMapProto, &gdpb.Coordinate{X: 2, Y: 1})
 	if err != nil {
@@ -160,17 +143,79 @@ func TestDoTick(t *testing.T) {
 		t.Fatalf("AddEntity() = %v, want = nil", err)
 	}
 
+	var eg errgroup.Group
+	var streamResponsesMux sync.Mutex
+	var streamResponses []*apipb.StreamDataResponse
 	var cids []string
+	chs := map[string]<-chan *apipb.StreamDataResponse{}
 	for i := 0; i < nClients; i++ {
+		// Add client -- emulate AddClient gRPC call.
 		cid, err := e.AddClient()
 		if err != nil {
 			t.Fatalf("AddClient() = _, %v, want = nil", err)
 		}
 		cids = append(cids, cid)
+
+		// As in the StreamData gRPC endpoint, first mark the client as
+		// reachable.
+		if err := e.StartClientStream(cids[i]); err != nil {
+			t.Fatalf("StartClientStream() = %v, want = nil", err)
+		}
+
+		ch, err := e.ClientChannel(cid)
+		if err != nil {
+			t.Fatalf("ClientChannel() = _, %v, want = _, nil", err)
+		}
+
+		chs[cid] = ch
+	}
+
+	// Listen for the initial tick execution, which will add the scheduled
+	// entity.
+	for _, ch := range chs {
+		ch := ch
+		eg.Go(func() error {
+			m := <-ch
+
+			streamResponsesMux.Lock()
+			defer streamResponsesMux.Unlock()
+
+			streamResponses = append(streamResponses, m)
+			return nil
+		})
+	}
+
+	eg.Go(e.doTick)
+
+	if err := eg.Wait(); err != nil {
+		t.Fatalf("Wait() = %v, want = nil", err)
+	}
+
+	if gotLen := len(streamResponses); gotLen != nClients {
+		t.Fatalf("len() = %v, want = %v", gotLen, nClients)
+	}
+
+	eid := streamResponses[0].GetEntities()[0].GetEntityId()
+	streamResponses = nil
+
+	want := &apipb.StreamDataResponse{
+		Tick: t0 + 2,
+		Curves: []*gdpb.Curve{{
+			EntityId: eid,
+			Type:     gcpb.CurveType_CURVE_TYPE_LINEAR_MOVE,
+			Category: gcpb.CurveCategory_CURVE_CATEGORY_MOVE,
+			Data: []*gdpb.CurveDatum{
+				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 0, Y: 0}}},
+				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 1, Y: 0}}},
+				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 2, Y: 0}}},
+				{Datum: &gdpb.CurveDatum_PositionDatum{&gdpb.Position{X: 3, Y: 0}}},
+			},
+			Tick: t0 + 2,
+		}},
 	}
 
 	if err := e.AddMoveCommands(&apipb.MoveRequest{
-		Tick:        t1,
+		Tick:        t0 + 2,
 		ClientId:    cids[0],
 		EntityIds:   []string{eid},
 		Destination: dest,
@@ -179,26 +224,9 @@ func TestDoTick(t *testing.T) {
 		t.Fatalf("AddMoveCommands() = %v, want = nil", err)
 	}
 
-	// Connect to server and signal intent to start listening for messages.
-	for i := 0; i < nClients; i++ {
-		if err := e.StartClientStream(cids[i]); err != nil {
-			t.Fatalf("StartClientStream() = %v, want = nil", err)
-		}
-	}
-
-	var eg errgroup.Group
-
-	var streamResponsesMux sync.Mutex
-	var streamResponses []*apipb.StreamDataResponse
-	for i := 0; i < nClients; i++ {
-		ch, err := e.ClientChannel(cids[i])
-		if err != nil {
-			t.Fatalf("ClientChannel() = _, %v, want = _, nil", err)
-		}
-		// Assuming all clients will receive messages in a timely
-		// manner. Start listening for messages before the tick starts
-		// to guarantee we will recieve a message during
-		// broadcastCurves.
+	// Listen for the move command broadcast.
+	for _, ch := range chs {
+		ch := ch
 		eg.Go(func() error {
 			m := <-ch
 
@@ -221,11 +249,9 @@ func TestDoTick(t *testing.T) {
 			want,
 			streamResponse,
 			protocmp.Transform(),
-			protocmp.IgnoreFields(&apipb.StreamDataResponse{}, "tick"),
 			protocmp.IgnoreFields(&gdpb.CurveDatum{}, "tick"),
 		); diff != "" {
 			t.Errorf("<-e.ClientChannel() mismatch (-want +got):\n%v", diff)
 		}
 	}
 }
-*/
