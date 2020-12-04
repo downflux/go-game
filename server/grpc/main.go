220fb06ae216f5911eee259c1468827e27e166db
Minke Zhang <minke.zhang@gmail.com>
2020-12-03 20:08:38 -0800

    Bug fix producer Visitor to create UUID per entity

--- server/grpc/main.go
+++ server/grpc/main.go
@@ -55,7 +55,8 @@ func main() {
 	apipb.RegisterDownFluxServer(s, downFluxServer)
 
 	downFluxServer.Executor().AddEntity(gcpb.EntityType_ENTITY_TYPE_TANK, &gdpb.Position{X: 1, Y: 1})
+	downFluxServer.Executor().AddEntity(gcpb.EntityType_ENTITY_TYPE_TANK, &gdpb.Position{X: 2, Y: 1})
 
 	go s.Serve(lis)
-	downFluxServer.Executor().Run()
+	log.Println(downFluxServer.Executor().Run())
 }
