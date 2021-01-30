af0d424164771684a632b1b579a21b9a05a26a99
Minke Zhang <minke.zhang@gmail.com>
2021-01-29 23:31:29 -0800

    Add Status client tests

---
+++ client/DownFlux/Assets/Engine/TestData/Status.cs
@@ -0,0 +1,24 @@
+﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EngineTestAssembly")]
+
+namespace DF.Test.Data
+{
+    internal static class StatusTestData
+    {
+        internal static DF.Game.API.Data.ServerStatus pb
+        {
+            get => new DF.Game.API.Data.ServerStatus
+            {
+                Tick = 10,
+                StartTime = new Google.Protobuf.WellKnownTypes.Timestamp
+                {
+                    Seconds = 946684800  // Jan 1, 2000
+                },
+                TickDuration = new Google.Protobuf.WellKnownTypes.Duration
+                {
+                    Nanos = 100000000  // 100ms
+                },
+                IsStarted = true
+            };
+        }
+    }
+}
