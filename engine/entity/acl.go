cc058984e399874672398bf15627ce507bdabd4c
Minke Zhang <minke.zhang@gmail.com>
2021-02-28 21:04:35 -0800

    Remove client ID reference in ACL

--- engine/entity/acl.go
+++ engine/entity/acl.go
@@ -18,9 +18,8 @@ type ACL struct {
 	acl      ACLType
 }
 
-func New(cid id.ClientID, acl ACLType) *ACL {
+func New(acl ACLType) *ACL {
 	return &ACL{
-		clientID: cid,
 		acl:      acl,
 	}
 }
@@ -28,20 +27,8 @@ func New(cid id.ClientID, acl ACLType) *ACL {
 func (a ACL) PublicWritable() bool { return a.acl&PublicWritable == PublicWritable }
 func (a ACL) ClientWritable() bool { return a.acl&ClientWritable == ClientWritable }
 
-func (a ACL) Validate(c *gdpb.ClientID) bool {
-	return a.PublicWritable() || (a.validateClientWritable(
-		id.ClientID(c.GetClientId())))
-}
-
-func (a ACL) validateClientWritable(c id.ClientID) bool {
-	return a.ClientWritable() && c == a.clientID
-}
-
 func (a ACL) Export() *gdpb.ACL {
 	return &gdpb.ACL{
-		Owner: &gdpb.ClientID{
-			ClientId: a.clientID.Value(),
-		},
 		PublicWritable: a.PublicWritable(),
 		ClientWritable: a.ClientWritable(),
 	}
